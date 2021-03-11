e86864852f9812ba49b4e076a4814027ca86f66f
Flávio Augusto <facmachado@gmail.com>
2021-03-11 19:24:26 -0300

    hexorg optimized

--- hexorg.sh
+++ hexorg.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #
-#  hexorg.sh - file hexdump with real origin address settable
+#  hexorg.sh - file hex dump with user-defined origin address (offset)
 #              (good for use with retrocomputing)
 #
 #  Copyright (c) 2021 Flavio Augusto (@facmachado)
@@ -13,8 +13,8 @@
 #
 # Checks some dependencies
 #
-if (($(tput cols) < 80 || $(tput lines) < 10)); then
-  echo 'Error: Console screen below 80 columns x 10 lines' >&2
+if (($(tput cols) < 80 || $(tput lines) < 24)); then
+  echo 'Error: screen below 80 columns x 24 lines' >&2
   exit 1
 elif [ ! "$(command -v less)" ]; then
   echo 'Error: less not installed' >&2
@@ -25,37 +25,29 @@ elif [ ! "$(command -v xxd)" ]; then
 fi
 
 #
-# Shows help
+# Checks if minimum argument is defined
 #
-function usage() {
-  echo "Usage: $(basename "$0") <-f file> [-o hex_addr]"
-}
-
-#
-# Checks args
-#
-while (("$#")); do
-  case $1 in
-    -f)  file=$2                                  ;;
-    -o)  org=${2:-0} && org=0x${org//[^0-9a-f]/}  ;;
-    *)   usage && exit 0                          ;;
-  esac
-  shift 2
-done
+if ((${#*} < 1)); then
+  echo "Usage: $(basename "$0") <file> [hex_addr]"
+  exit 1
+fi
 
 #
 # Checks if file exists
 #
-if [ ! "$file" ]; then
-  usage && exit 0
-elif [ ! -f "$file" ]; then
+if [ ! -f "$1" ]; then
   echo 'Error: file not found' >&2
   exit 1
 fi
 
 #
-# Opens file for analysis
+# Formats offset as value for xxd argument
 #
-xxd -g1 -o "$org" "$file" | less -KRP "(Q) QUIT  \
-00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f  \
-0123456789abcdef"
+org=$(sed 's/^0x//;s/[^0-9a-f]//g' <<<"$2")
+org=${org:-0}
+
+#
+# Opens file for analysis with less
+#
+less -KRP "--ADDR--  00 01 02 03 04 05 06 07 08 09 0a 0b 0c 0d 0e 0f  \
+0123456789abcdef" < <(xxd -g1 -o "0x$org" "$1" 2>/dev/null)
